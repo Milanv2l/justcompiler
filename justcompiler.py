2fb61040daf2c6d33e47e1d4b0ae8103963e00f6
Milanv_ <58265883+Milanv2l@users.noreply.github.com>
2026-06-06 22:52:07 +0200

    Update justcompiler.py

--- justcompiler.py
+++ justcompiler.py
@@ -12,7 +12,7 @@ from core import UI, t
 import baremetal
 
 # --- JUSTCOMPILER VERSION ---
-VERSION = "1.0.5"
+VERSION = "1.0.6"
 
 def init_terminal_colors():
     """Enables ANSI escape sequences for coloring in the Windows terminal."""
