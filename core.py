270a3d0837ba99775c1a909af5ab1882bd2c93eb
Milanv_ <58265883+Milanv2l@users.noreply.github.com>
2026-06-07 08:33:02 +0200

    Update core.py

--- core.py
+++ core.py
@@ -58,6 +58,7 @@ class UI:
     YELLOW = "\033[33m"
     GREEN = "\033[32m"
     RED = "\033[31m"
+    BLUE = "\033[34m"     # <-- Voeg deze regel toe!
     MAGENTA = "\033[35m"
     RESET = "\033[0m"
     BOLD = "\033[1m"
